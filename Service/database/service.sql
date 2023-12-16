cb777e2e66e72834bb31290c722d5ded09cb0a61
Relvin Arsenio <88470966+RelvinArsenio@users.noreply.github.com>
2023-12-17 00:07:33 +0700

    Update service.sql

--- Service/database/service.sql
+++ Service/database/service.sql
@@ -11,24 +11,22 @@ CREATE DATABASE IF NOT EXISTS `service` DEFAULT CHARACTER SET utf8mb4 COLLATE ut
 USE `service`;
 
 CREATE TABLE `jasa` (
-  `id` int(11) NOT NULL,
+  `id_jasa` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `notelp` varchar(15) NOT NULL,
   `jenis_service` varchar(50) NOT NULL,
   `tarif_service` int(11) NOT NULL,
   `biaya_tambahan` int(11) NOT NULL,
-  `total_bayar` int(11) GENERATED ALWAYS AS (`tarif_service` + `biaya_tambahan`) VIRTUAL
+  `total_bayar` int(11) GENERATED ALWAYS AS (`tarif_service` + `biaya_tambahan`) VIRTUAL,
+  PRIMARY KEY (`id_jasa`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `jasa` (`id`, `nama`, `notelp`, `jenis_service`, `tarif_service`, `biaya_tambahan`) VALUES
+INSERT INTO `jasa` (`id_jasa`, `nama`, `notelp`, `jenis_service`, `tarif_service`, `biaya_tambahan`) VALUES
 (1, 'Relvin Arsenio', '081291910202', 'Perbaikan Software', 200000, 55000),
 (2, 'Rino Aldera', '081234576796', 'Penggantian Mesin', 1000000, 250000),
 (3, 'Rizky Juniardi', '0812345678', 'Penggantian Baterai', 350000, 0),
 (4, 'Valian Ardafi', '08123456345', 'Perbaikan Layar', 500000, 0);
 
-
-ALTER TABLE `jasa`
-  ADD PRIMARY KEY (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
