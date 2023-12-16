33a75c6eaf3a03ac8640ac9a32c70796396de491
Relvin Arsenio <88470966+RelvinArsenio@users.noreply.github.com>
2023-12-17 00:05:50 +0700

    Update tagihan_sekolah.sql

--- tagihan_sekolah/database/tagihan_sekolah.sql
+++ tagihan_sekolah/database/tagihan_sekolah.sql
@@ -19,24 +19,27 @@ INSERT INTO `login` (`Username`, `Password`) VALUES
 ('admin1', '123');
 
 CREATE TABLE `pembayaran` (
-  `id` int(11) NOT NULL,
+  `id_pembayaran` int(11) NOT NULL,
   `tagihan_id` int(11) NOT NULL,
   `jumlah_bayar` decimal(10,2) NOT NULL,
-  `tanggal_bayar` date NOT NULL
+  `tanggal_bayar` date NOT NULL,
+  PRIMARY KEY (`id_pembayaran`),
+  KEY `tagihan_id` (`tagihan_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `pembayaran` (`id`, `tagihan_id`, `jumlah_bayar`, `tanggal_bayar`) VALUES
+INSERT INTO `pembayaran` (`id_pembayaran`, `tagihan_id`, `jumlah_bayar`, `tanggal_bayar`) VALUES
 (1, 1, 2500000.00, '2023-06-28'),
 (2, 2, 2500000.00, '2023-07-04');
 
 CREATE TABLE `siswa` (
-  `id` int(11) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `jenis_kelamin` set('Laki Laki','Perempuan') NOT NULL,
-  `kelas` varchar(10) NOT NULL
+  `kelas` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_siswa`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `siswa` (`id`, `nama`, `jenis_kelamin`, `kelas`) VALUES
+INSERT INTO `siswa` (`id_siswa`, `nama`, `jenis_kelamin`, `kelas`) VALUES
 (1, 'Alvin Grayson', 'Laki Laki', '10A'),
 (2, 'Aksa', 'Laki Laki', '12B'),
 (3, 'Alteza', 'Laki Laki', '10E'),
@@ -47,14 +50,16 @@ INSERT INTO `siswa` (`id`, `nama`, `jenis_kelamin`, `kelas`) VALUES
 (8, 'Senliana Siel', 'Laki Laki', '11A');
 
 CREATE TABLE `tagihan` (
-  `id` int(11) NOT NULL,
+  `id_tagihan` int(11) NOT NULL,
   `siswa_id` int(11) NOT NULL,
   `jenis_tagihan` varchar(50) NOT NULL,
   `jumlah` decimal(10,2) NOT NULL,
-  `tanggal_tagihan` date NOT NULL
+  `tanggal_tagihan` date NOT NULL,
+  PRIMARY KEY (`id_tagihan`),
+  KEY `siswa_id` (`siswa_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `tagihan` (`id`, `siswa_id`, `jenis_tagihan`, `jumlah`, `tanggal_tagihan`) VALUES
+INSERT INTO `tagihan` (`id_tagihan`, `siswa_id`, `jenis_tagihan`, `jumlah`, `tanggal_tagihan`) VALUES
 (1, 1, 'Biaya Sekolah', 2500000.00, '2023-06-26'),
 (2, 5, 'Study Tour', 2500000.00, '2023-07-05'),
 (3, 8, 'Pendaftaran', 200000.00, '2023-04-01'),
@@ -63,22 +68,19 @@ INSERT INTO `tagihan` (`id`, `siswa_id`, `jenis_tagihan`, `jumlah`, `tanggal_tag
 
 
 ALTER TABLE `pembayaran`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `tagihan_id` (`tagihan_id`);
+  DROP PRIMARY KEY,
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`tagihan_id`) REFERENCES `tagihan` (`id_tagihan`);
 
 ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`id`);
+  DROP PRIMARY KEY,
+  ADD PRIMARY KEY (`id_siswa`);
 
 ALTER TABLE `tagihan`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `siswa_id` (`siswa_id`);
+  DROP PRIMARY KEY,
+  ADD PRIMARY KEY (`id_tagihan`),
+  ADD CONSTRAINT `tagihan_ibfk_1` FOREIGN KEY (`siswa_id`) REFERENCES `siswa` (`id_siswa`);
 
-
-ALTER TABLE `pembayaran`
-  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`tagihan_id`) REFERENCES `tagihan` (`id`);
-
-ALTER TABLE `tagihan`
-  ADD CONSTRAINT `tagihan_ibfk_1` FOREIGN KEY (`siswa_id`) REFERENCES `siswa` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
